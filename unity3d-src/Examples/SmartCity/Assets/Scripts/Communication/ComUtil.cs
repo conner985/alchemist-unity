c37e64ad1d587b3fb828695fd9650167781819e7
Conner985 <Conner985@Conner-Laptop>
2017-05-02 19:00:01 +0200

    added an example as a case study for the interaction alchemist-unity

---
+++ unity3d-src/Examples/SmartCity/Assets/Scripts/Communication/ComUtil.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using System.Threading;
+
+public class ComUtil : MonoBehaviour
+{
+    public delegate void ResponseArrivedGET(string response);
+    public event ResponseArrivedGET responseArrivedGET;
+
+    public delegate void ResponseArrivedPOST(string response);
+    public event ResponseArrivedPOST responseArrivedPOST;
+
+    
+    #region ASYNC COMM
+
+    public void RequestGET_Async(string url)
+	{
+		UnityWebRequest req = UnityWebRequest.Get(url);
+		StartCoroutine(SendGETRequestAndWait(req));
+	}
+
+	public void RequestPOSTNodes_Async<T>(string url, NodesDescriptor<T> nodes, string postData) where T : ISimNode
+    {
+		string json_string = NodesDescriptor<T>.CreateJsonString(nodes);
+
+		UnityWebRequest req = UnityWebRequest.Post(url,postData);
+
+		byte[] json_bytes = System.Text.Encoding.UTF8.GetBytes(json_string);
+
+		UploadHandlerRaw uphandler = new UploadHandlerRaw(json_bytes);
+		uphandler.contentType = "application/json";
+
+		req.uploadHandler = uphandler;
+		
+		StartCoroutine(SendPOSTRequestAndWait(req));
+    }
+
+
+    private IEnumerator SendGETRequestAndWait(UnityWebRequest req)
+    {
+        yield return req.Send();
+        responseArrivedGET(req.downloadHandler.text);
+    }
+
+    private IEnumerator SendPOSTRequestAndWait(UnityWebRequest req)
+    {
+        yield return req.Send();
+        responseArrivedPOST(req.downloadHandler.text);
+    }
+
+    #endregion
+
+    #region SYNC COMM
+
+    public NodesDescriptor<T> RequestGET_Sync<T>(string url) where T : ISimNode
+    {
+        UnityWebRequest req = UnityWebRequest.Get(url);
+
+        AsyncOperation op = req.Send();
+        while (!op.isDone) { Thread.Sleep(100); }
+
+        return JsonUtility.FromJson<NodesDescriptor<T>>(req.downloadHandler.text);
+    }
+
+    public string RequestPOSTNodes_Sync(string url, int n_nodes, string postData, string progType)
+    {
+        InitComm initComm = new InitComm("init", n_nodes, progType);
+        string json_string = JsonUtility.ToJson(initComm);
+
+        UnityWebRequest req = UnityWebRequest.Post(url, postData);
+
+        byte[] json_bytes = System.Text.Encoding.UTF8.GetBytes(json_string);
+
+        UploadHandlerRaw uphandler = new UploadHandlerRaw(json_bytes);
+        uphandler.contentType = "application/json";
+
+        req.uploadHandler = uphandler;
+
+        AsyncOperation op = req.Send();
+        while (!op.isDone) { Thread.Sleep(100); }
+
+        return req.downloadHandler.text;
+    }
+
+    #endregion
+}
---
+++ unity3d-src/Examples/SmartCity/Assets/Scripts/Node/Others/GradientNodesCollector.cs
@@ -0,0 +1,145 @@
+using UnityEngine;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System;
+
+public class GradientNodesCollector : MonoBehaviour {
+
+    [SerializeField]
+    private ComUtil com;
+    [SerializeField]
+    private int post2getTime = 1000;
+    [SerializeField]
+    private int get2postTime = 500;
+
+    private Stopwatch stopWatchGET;
+    private Stopwatch stopWatchPOST;
+
+    private NodesDescriptor<GradientNode> nd;
+    private IList<AbstractBehaviourNode> behaviourNodes;
+
+    private System.Random rnd;
+    private bool doComm;
+
+    void Start ()
+    {
+        Application.runInBackground = true;
+        // pause the simulation
+        Time.timeScale = 0;
+
+        rnd = new System.Random(DateTime.Now.Millisecond);
+
+        behaviourNodes = FindObjectsOfType(typeof(AbstractBehaviourNode)) as IList<AbstractBehaviourNode>;
+
+        stopWatchGET = Stopwatch.StartNew();
+        stopWatchGET.Reset();
+        stopWatchPOST = Stopwatch.StartNew();
+        stopWatchPOST.Reset(); 
+
+        com.RequestPOSTNodes_Sync("http://localhost:8080/", behaviourNodes.Count, "","GRADIENT");
+        nd = com.RequestGET_Sync<GradientNode>("http://localhost:8080/");
+        CreateAllNodes();
+
+        // start the simulation
+        Time.timeScale = 1;
+        stopWatchPOST.Start();
+    }
+
+    void Update ()
+    {
+        if (stopWatchPOST.Elapsed.TotalMilliseconds > post2getTime)
+        {
+            stopWatchPOST.Reset();
+            CollectAllNodes();
+            SendNodes();
+        }
+
+        else if (stopWatchGET.Elapsed.TotalMilliseconds > get2postTime)
+        {
+            stopWatchGET.Reset();
+            ReceiveNodes();
+        }
+    }
+
+    private void CreateAllNodes()
+    {
+        if (nd.GetNodes().Count != behaviourNodes.Count)
+        {
+            //errore!!!
+            UnityEngine.Debug.Log("unity nodes: " + behaviourNodes.Count);
+            UnityEngine.Debug.Log("alchemist nodes: " + nd.GetNodes().Count);
+            UnityEngine.Debug.LogError("Inconsistent number of nodes between Unity and Alchemist");
+            return;
+        }
+
+        int i;
+        for (i = 0; i < behaviourNodes.Count; i++)
+        {
+            int id = nd.GetNodes()[i].GetID();
+            behaviourNodes[i].CreateNode(id, SimNodeTypes.type.GRADIENT);
+        }
+
+    }
+
+    private void SendNodes()
+    {
+        com.RequestPOSTNodes_Async("http://localhost:8080/ ", nd, "");
+        com.responseArrivedPOST += ResponseArrivedPOST;
+    }
+
+    private void ReceiveNodes()
+    {
+
+        com.RequestGET_Async("http://localhost:8080/ ");
+        com.responseArrivedGET += ResponseArrivedGET;
+    }
+
+    private void ResponseArrivedGET(string response)
+    {
+        com.responseArrivedGET -= ResponseArrivedGET;
+
+        NodesDescriptor<GradientNode> newND = NodesDescriptor<GradientNode>.CreateNodesDescriptorFromJsonString(response);
+        UpdateAllNodes(newND);
+        stopWatchPOST.Start();
+    }
+
+    private void ResponseArrivedPOST(string response)
+    {
+        com.responseArrivedPOST -= ResponseArrivedPOST;
+        stopWatchGET.Start();
+    }
+
+    private void CollectAllNodes()
+    {
+        if (rnd.NextDouble() < 0.1f)
+        {
+            
+        }
+        behaviourNodes = FindObjectsOfType(typeof(AbstractBehaviourNode)) as IList<AbstractBehaviourNode>;
+
+        nd.ClearNodes();
+        nd.SetType("step");
+        foreach(AbstractBehaviourNode bn in behaviourNodes)
+        {
+            nd.AddNode(bn.GetNode());
+        }
+    }
+
+    private void UpdateAllNodes(NodesDescriptor<GradientNode> newND)
+    {
+        CollectAllNodes();
+        IDictionary<int, GradientNode> newDict = newND.ToDictionary();
+        int i = 0;
+        for(i = 0; i < nd.GetNodes().Count; i++)
+        {
+            GradientNode newSimNode;
+            var node = nd.GetNodes()[i];
+            if (newDict.TryGetValue(node.GetID(), out newSimNode))
+            {
+                var molecules = newSimNode.GetMolecules();
+                node.SetMolecules(molecules);
+            }
+        }
+
+    }
+}
